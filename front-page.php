d85e66c03ce86b8d7edc6160f9716fcf355a6d11
Mark-Luigi <markluigibatoks@gmail.com>
2025-08-01 21:44:17 -0230

    add: banner

--- front-page.php
+++ front-page.php
@@ -1,4 +1,4 @@
-<div class="lg:pt-6 bg-gradient-light-blue-10">
+<div class="lg:pt-6 xl:pr-14 bg-gradient-light-blue-10">
 <?php
     get_header();
     get_template_part("/partials/front-page/banner");
--- partials/front-page/banner.php
+++ partials/front-page/banner.php
@@ -1,13 +1,14 @@
-<div class="main-box p-0">
-    <div class="w-full h-full">
-        <img class="w-full h-full object-cover" src="<?php echo get_template_directory_uri();?>/assets/images/about-us/placeholder.svg" alt="placeholder"/>
-    </div>
-    <div class="main-box bg-black lg:bg-transparent lg:absolute lg:top-1/2 lg:-translate-y-1/2 lg:left-0">
-        <div class="wrapper">
-            <div class="w-full text-center lg:text-left text-white">
-                <span>Sub heading text-center</span>
-                <h2 class="text-4xl">Lorem Ipsum text</h2>
-                <a href="#" class="button-nav-link block max-w-max mx-auto lg:mx-0">Click here</a>
+<div class="main-box lg:py-39 py-10">
+    <div class="wrapper">
+        <div class="flex lg:flex-row flex-col items-center justify-between gap-10">
+            <div class="lg:max-w-[585px] w-full text-center lg:text-left">
+                <h2 class="text-primary lg:mb-8 mb-5 xl:text-h1 lg:text-h2 md:text-h4 text-h5">Run Your Entire Customer Support Remotely</h2>
+                <p>Lorem ipsum dolohr sit amet, consectetur adipiscing elit. Vestibulum vitae nunc vitae neque</p>
+                <a href="#" class="mt-12 button-blue block max-w-max mx-auto lg:mx-0">Get Started For Free</a>
+            </div>
+
+            <div class="xl:-mr-14 max-w-max mx-auto">
+                <img class="object-cover w-full h-full" src="<?php echo get_template_directory_uri();?>/assets/images/front-page/banner.png" alt="site health data"/>
             </div>
         </div>
     </div>

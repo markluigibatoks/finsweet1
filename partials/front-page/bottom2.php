e80c64958adcdcfff76395b66ca07cacbc301a0e
Mark-Luigi <markluigibatoks@gmail.com>
2025-08-04 08:46:27 -0230

    add: bottom2

---
+++ partials/front-page/bottom2.php
@@ -0,0 +1,95 @@
+<div class="main-box bg-white">
+    <div class="wrapper">
+        <div class="w-full text-center lg:text-left">
+            <h2 class="lg:text-h2 md:text-h4 text-h5 text-primary">Customer Testimonials</h2>
+        </div>
+        <div class="mt-12 grid gap-8 grid-cols-1 lg:grid-cols-3">
+          <div class="rounded-xl p-8 bg-gradient-light-blue-20 w-full text-center lg:text-left">
+            <div class="mb-7 flex gap-4 items-center">
+              <div class="max-w-max">
+                <img class="rounded-full object-cover w-full" src="<?php echo get_template_directory_uri();?>/assets/images/front-page/bottom2-img1.png" alt="breanna parker"/>
+              </div>
+              <div>
+                <h5 class="text-[22px] font-medium leading-[28px] text-black">Breanna Parker</h5>
+                <div class="mt-2 flex gap-2 items-center">
+                  <div class="flex items-center justify-center">
+                    <?php echo file_get_contents(get_template_directory() . '/assets/images/star.svg'); ?>
+                  </div>
+                  <div class="flex items-center justify-center">
+                    <?php echo file_get_contents(get_template_directory() . '/assets/images/star.svg'); ?>
+                  </div>
+                  <div class="flex items-center justify-center">
+                    <?php echo file_get_contents(get_template_directory() . '/assets/images/star.svg'); ?>
+                  </div>
+                  <div class="flex items-center justify-center">
+                    <?php echo file_get_contents(get_template_directory() . '/assets/images/star.svg'); ?>
+                  </div>
+                  <div class="flex items-center justify-center">
+                    <?php echo file_get_contents(get_template_directory() . '/assets/images/star.svg'); ?>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <p class="leading-[32px] text-primary">Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Ut ornare gravida tempor. Nunc tristique libero eget nulla.</p>
+          </div>
+
+          <div class="rounded-xl p-8 bg-gradient-light-blue-20 w-full text-center lg:text-left">
+            <div class="mb-7 flex gap-4 items-center">
+              <div class="max-w-max">
+                <img class="rounded-full object-cover w-full" src="<?php echo get_template_directory_uri();?>/assets/images/front-page/bottom2-img2.png" alt="Ana Oleynkova"/>
+              </div>
+              <div>
+                <h5 class="text-[22px] font-medium leading-[28px] text-black">Ana Oleynkova</h5>
+                <div class="mt-2 flex gap-2 items-center">
+                  <div class="flex items-center justify-center">
+                    <?php echo file_get_contents(get_template_directory() . '/assets/images/star.svg'); ?>
+                  </div>
+                  <div class="flex items-center justify-center">
+                    <?php echo file_get_contents(get_template_directory() . '/assets/images/star.svg'); ?>
+                  </div>
+                  <div class="flex items-center justify-center">
+                    <?php echo file_get_contents(get_template_directory() . '/assets/images/star.svg'); ?>
+                  </div>
+                  <div class="flex items-center justify-center">
+                    <?php echo file_get_contents(get_template_directory() . '/assets/images/star.svg'); ?>
+                  </div>
+                  <div class="flex items-center justify-center">
+                    <?php echo file_get_contents(get_template_directory() . '/assets/images/star.svg'); ?>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <p class="leading-[32px] text-primary">Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Ut ornare gravida tempor. Nunc tristique libero eget nulla.</p>
+          </div>
+
+          <div class="rounded-xl p-8 bg-gradient-light-blue-20 w-full text-center lg:text-left">
+            <div class="mb-7 flex gap-4 items-center">
+              <div class="max-w-max">
+                <img class="rounded-full object-cover w-full" src="<?php echo get_template_directory_uri();?>/assets/images/front-page/bottom2-img3.png" alt="Andy Smith"/>
+              </div>
+              <div>
+                <h5 class="text-[22px] font-medium leading-[28px] text-black">Andy Smith</h5>
+                <div class="mt-2 flex gap-2 items-center">
+                  <div class="flex items-center justify-center">
+                    <?php echo file_get_contents(get_template_directory() . '/assets/images/star.svg'); ?>
+                  </div>
+                  <div class="flex items-center justify-center">
+                    <?php echo file_get_contents(get_template_directory() . '/assets/images/star.svg'); ?>
+                  </div>
+                  <div class="flex items-center justify-center">
+                    <?php echo file_get_contents(get_template_directory() . '/assets/images/star.svg'); ?>
+                  </div>
+                  <div class="flex items-center justify-center">
+                    <?php echo file_get_contents(get_template_directory() . '/assets/images/star.svg'); ?>
+                  </div>
+                  <div class="flex items-center justify-center">
+                    <?php echo file_get_contents(get_template_directory() . '/assets/images/star.svg'); ?>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <p class="leading-[32px] text-primary">Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Ut ornare gravida tempor. Nunc tristique libero eget nulla.</p>
+          </div>
+        </div>
+    </div>
+</div>

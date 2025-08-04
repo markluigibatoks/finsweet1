efac1893b05369ff950ef0465a29109ec9e91be3
Mark-Luigi <markluigibatoks@gmail.com>
2025-08-04 09:14:46 -0230

    add: bottom3

---
+++ partials/front-page/bottom3.php
@@ -0,0 +1,49 @@
+<div class="main-box lg:py-32 py-10 bg-white">
+    <div class="wrapper max-w-[934px]">
+        <div class="w-full text-center lg:text-left">
+            <h2 class="lg:text-h2 md:text-h4 text-h5 text-primary">FAQ</h2>
+        </div>
+
+        <div class="grid grid-cols-1">
+          <details class="group w-full pl-5 sm:pl-16 sm:py-10 py-5 border-b border-[#EAEAEA]">
+            <summary class="relative list-none">
+              <span class="absolute -top-1 -left-10 lg:-left-16 group-open:text-blue text-2xl leading-[1.71] tracking-normal font-medium">01</span>
+              <h2 class="text-h5 text-black">Maecenas efficitur scelerisque lorem?</h2>
+            </summary>
+            <p class="mt-6">Nillam bibendum diam diam, maximus malesuada tortor volutpat sit amet. Curabitur volutpat feugiat tellus, sit amet faucibus massa vulputate lobortis. Vestibulum malesuada ex dolor,</p>
+          </details>
+
+          <details class="group w-full pl-5 sm:pl-16 sm:py-10 py-5 border-b border-[#EAEAEA]">
+            <summary class="relative list-none">
+              <span class="absolute -top-1 -left-10 lg:-left-16 group-open:text-blue text-2xl leading-[1.71] tracking-normal font-medium">02</span>
+              <h2 class="text-h5 text-black">Pellentesque ultricies nulla sit amet ipsum kra pellentesque?</h2>
+            </summary>
+            <p class="mt-6">Nillam bibendum diam diam, maximus malesuada tortor volutpat sit amet. Curabitur volutpat feugiat tellus, sit amet faucibus massa vulputate lobortis. Vestibulum malesuada ex dolor,</p>
+          </details>
+
+          <details class="group w-full pl-5 sm:pl-16 sm:py-10 py-5 border-b border-[#EAEAEA]">
+            <summary class="relative list-none">
+              <span class="absolute -top-1 -left-10 lg:-left-16 group-open:text-blue text-2xl leading-[1.71] tracking-normal font-medium">03</span>
+              <h2 class="text-h5 text-black">Nullam bibendum diam diam, maximus malesuada?</h2>
+            </summary>
+            <p class="mt-6">Nillam bibendum diam diam, maximus malesuada tortor volutpat sit amet. Curabitur volutpat feugiat tellus, sit amet faucibus massa vulputate lobortis. Vestibulum malesuada ex dolor,</p>
+          </details>
+
+          <details class="group w-full pl-5 sm:pl-16 sm:py-10 py-5 border-b border-[#EAEAEA]">
+            <summary class="relative list-none">
+              <span class="absolute -top-1 -left-10 lg:-left-16 group-open:text-blue text-2xl leading-[1.71] tracking-normal font-medium">04</span>
+              <h2 class="text-h5 text-black">Vivamus eleifend nec felis vel auctor?</h2>
+            </summary>
+            <p class="mt-6">Nillam bibendum diam diam, maximus malesuada tortor volutpat sit amet. Curabitur volutpat feugiat tellus, sit amet faucibus massa vulputate lobortis. Vestibulum malesuada ex dolor,</p>
+          </details>
+
+          <details class="group w-full pl-5 sm:pl-16 sm:py-10 py-5">
+            <summary class="relative list-none">
+              <span class="absolute -top-1 -left-10 lg:-left-16 group-open:text-blue text-2xl leading-[1.71] tracking-normal font-medium">05</span>
+              <h2 class="text-h5 text-black">Vivamus eleifend nec felis vel auctor?</h2>
+            </summary>
+            <p class="mt-6">Nillam bibendum diam diam, maximus malesuada tortor volutpat sit amet. Curabitur volutpat feugiat tellus, sit amet faucibus massa vulputate lobortis. Vestibulum malesuada ex dolor,</p>
+          </details>
+        </div>
+    </div>
+</div>

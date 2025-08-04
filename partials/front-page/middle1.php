093a12d0a2db1fac3a9aea1817f82e399bb082bf
Mark-Luigi <markluigibatoks@gmail.com>
2025-08-04 02:42:18 -0230

    add: middle1

--- partials/front-page/middle1.php
+++ partials/front-page/middle1.php
@@ -1,9 +1,29 @@
-<div class="main-box lg:py-30 py-10">
+<div class="main-box lg:pt-24 lg:pb-28 py-10">
     <div class="wrapper">
         <div class="w-full text-center">
-            <h2 class="text-4xl">Lorem Ipsum</h2>
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eligendi error, ad totam mollitia similique perferendis numquam commodi perspiciatis provident ipsa amet alias odit labore eaque adipisci aliquam rem dolores sed.</p>
-            <a href="#" class="button-primary mx-auto block mt-5 max-w-max">Click here</a>
+            <h2>10,000+ clients who are getting more replies</h2>
+        </div>
+
+        <div class="mt-12 flex gap-10 lg:gap-5 justify-between items-center overflow-auto">
+            <div class="flex items-center justify-center">
+                <?php echo file_get_contents(get_template_directory() . '/assets/images/front-page/middle1-img1.svg'); ?>
+            </div>
+
+            <div class="flex items-center justify-center">
+                <?php echo file_get_contents(get_template_directory() . '/assets/images/front-page/middle1-img2.svg'); ?>
+            </div>
+
+            <div class="flex items-center justify-center">
+                <?php echo file_get_contents(get_template_directory() . '/assets/images/front-page/middle1-img3.svg'); ?>
+            </div>
+
+            <div class="flex items-center justify-center">
+                <?php echo file_get_contents(get_template_directory() . '/assets/images/front-page/middle1-img4.svg'); ?>
+            </div>
+
+            <div class="flex items-center justify-center">
+                <?php echo file_get_contents(get_template_directory() . '/assets/images/front-page/middle1-img5.svg'); ?>
+            </div>
         </div>
     </div>
 </div>

611433f82f64e24e0f5d0175a5d968ec3e5fea39
Mark-Luigi <markluigibatoks@gmail.com>
2025-08-04 03:34:29 -0230

    add: middle2:

--- front-page.php
+++ front-page.php
@@ -6,6 +6,7 @@
 </div>
 <?php 
     get_template_part("/partials/front-page/middle1");
+    get_template_part("/partials/front-page/middle2");
     get_template_part("/partials/front-page/main");
     get_template_part("/partials/front-page/bottom1");
     get_footer();
---
+++ partials/front-page/middle2.php
@@ -0,0 +1,41 @@
+<div class="main-box bg-white lg:pb-32 pb-10">
+    <div class="wrapper">
+      <div class="flex lg:flex-row flex-col gap-10 xl:gap-28">
+        <div class="lg:max-w-[512px] w-full text-center lg:text-left">
+            <span class="text-paragraph text-blue">Why Choose Us?</span>
+            <h2 class="lg:text-h2 md:text-h4 text-h5 text-primary my-4">Premium features for competitive price</h2>
+            <p>Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Ut nec turpis tristique, egestas lacus at, cursus arcu</p>
+        </div>
+
+        <div class="sm:max-w-max sm:mx-auto grid grid-cols-1 sm:block sm:columns-2 gap-8">
+          <div class="sm:mt-9 sm:max-w-[260px] rounded-xl bg-gradient-light-blue-20 pt-12 px-10 pb-8 w-full">
+            <div class="flex items-center">
+              <?php echo file_get_contents(get_template_directory() . '/assets/images/front-page/middle2-icon1.svg'); ?>
+            </div>
+            <h2 class="max-w-40 mt-13 text-h5 leading-[36px] text-blue">Best In Market</h2>
+          </div>
+
+          <div class="sm:mt-9 sm:max-w-[260px] rounded-xl bg-gradient-light-red-20 pt-12 px-10 pb-8 w-full">
+            <div class="flex items-center">
+              <?php echo file_get_contents(get_template_directory() . '/assets/images/front-page/middle2-icon3.svg'); ?>
+            </div>
+            <h2 class="max-w-40 mt-13 text-h5 leading-[36px] text-red">Easy Analytics</h2>
+          </div>
+
+          <div class="sm:max-w-[260px] rounded-xl bg-gradient-light-blue-20 pt-12 px-10 pb-8 w-full">
+            <div class="flex items-center">
+              <?php echo file_get_contents(get_template_directory() . '/assets/images/front-page/middle2-icon2.svg'); ?>
+            </div>
+            <h2 class="max-w-40 mt-13 text-h5 leading-[36px] text-blue">Highly Secure</h2>
+          </div>
+
+          <div class="sm:mt-9 sm:max-w-[260px] rounded-xl bg-gradient-light-red-20 pt-12 px-10 pb-8 w-full">
+            <div class="flex items-center">
+              <?php echo file_get_contents(get_template_directory() . '/assets/images/front-page/middle2-icon4.svg'); ?>
+            </div>
+            <h2 class="max-w-40 mt-13 text-h5 leading-[36px] text-red">Priority Support</h2>
+          </div>
+        </div>
+      </div>
+    </div>
+</div>

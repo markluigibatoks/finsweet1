73d99321a50ab653f5f688bde92036f8630e485e
Mark-Luigi <markluigibatoks@gmail.com>
2025-08-04 05:20:16 -0230

    add: bottom1

--- partials/front-page/bottom1.php
+++ partials/front-page/bottom1.php
@@ -1,9 +1,66 @@
-<div class="main-box lg:py-30 py-10">
+<div class="main-box bg-white lg:py-32 py-10">
     <div class="wrapper">
-        <div class="w-full text-center">
-            <h2 class="text-4xl">Lorem Ipsum</h2>
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eligendi error, ad totam mollitia similique perferendis numquam commodi perspiciatis provident ipsa amet alias odit labore eaque adipisci aliquam rem dolores sed.</p>
-            <a href="#" class="button-primary mx-auto block mt-5 max-w-max">Click here</a>
+        <div class="flex flex-col md:flex-row gap-5 lg:gap-20 items-center">
+            <div class="bg-gradient-light-blue-20 p-5 w-full md:max-w-[731px] md:min-h-[523px] flex items-center justify-center rounded-[31px] md:rounded-br-none">
+                <div class="max-w-max mx-auto">
+                    <img class="object-cover w-full" src="<?php echo get_template_directory_uri();?>/assets/images/front-page/bottom1-img1.jpg" alt="a bar graph"/>
+                </div>
+            </div>
+
+            <div class="max-w-[512px] w-full text-center md:text-left">
+                <span class="text-blue">Features</span>
+                <h2 class="mb-6 mt-4 lg:text-h2 md:text-h4 text-h5 text-primary">Analytics at your fingertips</h2>
+                <p>Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Ut nec turpis tristique, egestas lacus at, cursus arcu</p>
+                <a href="<?php echo site_url('/product')?>" class="flex gap-3 mt-8 button-primary mx-auto md:mx-0 text-[#1A1A1A] hover:underline max-w-max">
+                    Learn More
+
+                    <div class="flex items-center justify-center">
+                        <?php echo file_get_contents(get_template_directory() . '/assets/images/arrow.svg'); ?>
+                    </div>
+                </a>
+            </div>
+        </div>
+
+        <div class="my-10 lg:my-32 xl:pl-27 flex flex-col lg:flex-row gap-5 lg:gap-17 items-center">
+            <div class="lg:order-2 bg-gradient-light-red-20 p-5 w-full md:max-w-[731px] md:min-h-[523px] flex items-center justify-center rounded-[31px] md:rounded-bl-none">
+                <div class="max-w-max mx-auto">
+                    <img class="object-cover w-full" src="<?php echo get_template_directory_uri();?>/assets/images/front-page/bottom1-img1.jpg" alt="a bar graph"/>
+                </div>
+            </div>
+    
+            <div class="lg:order-1 max-w-[512px] w-full text-center md:text-left">
+                <span class="text-blue">Features</span>
+                <h2 class="mb-6 mt-4 lg:text-h2 md:text-h4 text-h5 text-primary">Stay connected with your customers</h2>
+                <p>Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Ut nec turpis tristique, egestas lacus at, cursus arcu</p>
+                <a href="<?php echo site_url('/product')?>" class="flex gap-3 mt-8 button-primary mx-auto md:mx-0 text-[#1A1A1A] hover:underline max-w-max">
+                    Learn More
+
+                    <div class="flex items-center justify-center">
+                        <?php echo file_get_contents(get_template_directory() . '/assets/images/arrow.svg'); ?>
+                    </div>
+                </a>
+            </div>
+        </div>
+
+        <div class="flex flex-col md:flex-row gap-5 lg:gap-20 items-center">
+            <div class="bg-gradient-light-blue-20 p-5 w-full md:max-w-[731px] md:min-h-[523px] flex items-center justify-center rounded-[31px] md:rounded-br-none">
+                <div class="max-w-max mx-auto">
+                    <img class="object-cover w-full" src="<?php echo get_template_directory_uri();?>/assets/images/front-page/bottom1-img1.jpg" alt="a bar graph"/>
+                </div>
+            </div>
+
+            <div class="max-w-[512px] w-full text-center md:text-left">
+                <span class="text-blue">Features</span>
+                <h2 class="mb-6 mt-4 lg:text-h2 md:text-h4 text-h5 text-primary">Stay up to date with  live alerts</h2>
+                <p>Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Ut nec turpis tristique, egestas lacus at, cursus arcu</p>
+                <a href="<?php echo site_url('/product')?>" class="flex gap-3 mt-8 button-primary mx-auto md:mx-0 text-[#1A1A1A] hover:underline max-w-max">
+                    Learn More
+
+                    <div class="flex items-center justify-center">
+                        <?php echo file_get_contents(get_template_directory() . '/assets/images/arrow.svg'); ?>
+                    </div>
+                </a>
+            </div>
         </div>
     </div>
 </div>

2e9540951e71608f6b1cae07aa3b0935884aa7ac
Mark-Luigi <markluigibatoks@gmail.com>
2025-08-04 04:14:49 -0230

    add: main

--- partials/front-page/main.php
+++ partials/front-page/main.php
@@ -1,9 +1,11 @@
-<main class="main-box lg:py-30 py-10 bg-black text-white">
-    <div class="wrapper">
-        <div class="w-full text-center">
-            <h1 class="text-4xl">Lorem Ipsum</h1>
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eligendi error, ad totam mollitia similique perferendis numquam commodi perspiciatis provident ipsa amet alias odit labore eaque adipisci aliquam rem dolores sed.</p>
-            <a href="#" class="button-primary mx-auto block mt-5 max-w-max">Click here</a>
+<main class="main-box lg:px-0 lg:mb-36 bg-white text-white">
+    <div class="z-0 sticky top-0 sm:top-[inherit] sm:relative w-full max-w-max mx-auto">
+        <img class="sm:aspect-auto aspect-square object-cover w-full" src="<?php echo get_template_directory_uri();?>/assets/images/front-page/main-img1.jpg" alt="a woman is working happily"/>
+    </div>
+    <div class="z-10 relative wrapper">
+        <div class="lg:max-w-[1360px] lg:absolute lg:right-0 lg:-bottom-36 bg-blue px-5 py-10 xl:p-24 lg:flex gap-10 xl:gap-22 items-center w-full text-center lg:text-left">
+            <h1 class="lg:mb-0 mb-5 lg:max-w-[300px] lg:text-h2 md:text-h4 text-h5">Keeps Your Team Happy</h1>
+            <p class="lg:max-w-[589px]">Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Ut nec turpis tristique, egestas lacus at, cursus arcu. Vestibulum ante ipsum primis in.</p>
         </div>
     </div>
 </main>

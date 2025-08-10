edc691caaf42ec4a346213f5652db511e4f2b351
Mark-Luigi <markluigibatoks@gmail.com>
2025-08-10 03:05:21 -0230

    add: product/banner

---
+++ page-product.php
@@ -0,0 +1,18 @@
+<?php 
+/*  Template Name: Product Page
+    Template Post Type: page
+*/
+?>
+
+<div class="lg:pt-6 xl:pr-14 bg-gradient-light-blue-10">
+<?php
+    get_header();
+    get_template_part("/partials/product/banner");
+?>
+</div>
+
+<?php
+  get_template_part("/partials/product/section1");
+  
+  get_footer();
+?>
---
+++ partials/product/banner.php
@@ -0,0 +1,21 @@
+<main class="main-box lg:pt-24 lg:pb-30">
+    <div class="wrapper">
+      <div class="flex flex-col lg:flex-row items-center justify-between gap-10">
+        <div class="lg:max-w-[631px] w-full text-center lg:text-left">
+            <h2 class="mb-6 xl:text-h1 lg:text-h2 md:text-h4 text-h5 text-primary">Help your team to focus on priority tasks</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vitae nunc vitae neque .</p>
+            <a href="#" class="button-primary mx-auto lg:mx-0"></a>
+        </div>
+
+        <div class="max-w-[690px] flex gap-5 lg:gap-12 pr-12">
+          <div class="self-end relative max-w-max before:w-8 before:h-8 before:bg-blue before:absolute before:-top-16 before:-right-0 before:rounded-br-[10px]">
+            <img class="object-cover w-full" src="<?php echo get_template_directory_uri();?>/assets/images/product/banner-img1.png" alt="woman smiling"/>
+          </div>
+
+          <div class="relative max-w-max before:w-8 before:h-8 before:bg-red before:absolute before:top-0 before:-right-12 before:rounded-bl-[10px]">
+            <img class="object-cover w-full" src="<?php echo get_template_directory_uri();?>/assets/images/product/banner-img2.png" alt="woman smiling"/>
+          </div>
+        </div>
+      </div>
+    </div>
+</main>

fdbdd7a74b6f7f0f1512d730bc03c39008a3b985
Mark-Luigi <markluigibatoks@gmail.com>
2025-08-10 03:38:23 -0230

    add: product/section1

---
+++ partials/product/section1.php
@@ -0,0 +1,33 @@
+<div class="main-box bg-white lg:pt-32 pt-10">
+    <div class="wrapper">
+        <div class="lg:max-w-[782px] w-full text-center lg:text-left">
+            <h2 class="lg:text-h2 md:text-h4 text-h5 text-primary">Get the best out of your customer support with Finsweet</h2>
+        </div>
+
+        <div class="lg:mt-20 mt-10 grid grid-cols-1 place-items-center md:place-items-start sm:grid-cols-2 lg:grid-cols-3 gap-10">
+          <div class="max-w-[373px] w-full text-center lg:text-left">
+            <div class="lg:mx-0 mx-auto bg-gradient-light-blue-15 w-25 h-25 flex items-center justify-center rounded-xl">
+              <?php echo file_get_contents(get_template_directory() . '/assets/images/product/section1-icon1.svg'); ?>
+            </div>
+            <h2 class="text-h5 text-black mt-8 mb-4">Analytics</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vitae nunc vitae neque .</p>
+          </div>
+
+          <div class="max-w-[373px] w-full text-center lg:text-left">
+            <div class="lg:mx-0 mx-auto bg-gradient-light-blue-15 w-25 h-25 flex items-center justify-center rounded-xl">
+              <?php echo file_get_contents(get_template_directory() . '/assets/images/product/section1-icon2.svg'); ?>
+            </div>
+            <h2 class="text-h5 text-black mt-8 mb-4">Best Quality</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vitae nunc vitae neque .</p>
+          </div>
+
+          <div class="mx-auto col-span-1 sm:col-span-2 lg:col-span-1 max-w-[373px] w-full text-center lg:text-left">
+            <div class="lg:mx-0 mx-auto bg-gradient-light-blue-15 w-25 h-25 flex items-center justify-center rounded-xl">
+              <?php echo file_get_contents(get_template_directory() . '/assets/images/product/section1-icon3.svg'); ?>
+            </div>
+            <h2 class="text-h5 text-black mt-8 mb-4">More Clicks</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vitae nunc vitae neque .</p>
+          </div>
+        </div>
+    </div>
+</div>

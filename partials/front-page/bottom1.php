38195970f7366ef08b42ac106c5078921982826b
Mark-Luigi <markluigibatoks@gmail.com>
2025-08-10 05:09:22 -0230

    fix: bottom1 grid is not alternating the contents

--- partials/front-page/bottom1.php
+++ partials/front-page/bottom1.php
@@ -21,14 +21,14 @@
             </div>
         </div>
 
-        <div class="my-10 lg:my-32 xl:pl-27 flex flex-col lg:flex-row gap-5 lg:gap-17 items-center">
-            <div class="lg:order-2 bg-gradient-light-red-20 p-5 w-full md:max-w-[731px] md:min-h-[523px] flex items-center justify-center rounded-[31px] md:rounded-bl-none">
+        <div class="my-10 lg:my-32 xl:pl-27 flex flex-col md:flex-row gap-5 lg:gap-17 items-center">
+            <div class="md:order-2 bg-gradient-light-red-20 p-5 w-full md:max-w-[731px] md:min-h-[523px] flex items-center justify-center rounded-[31px] md:rounded-bl-none">
                 <div class="max-w-max mx-auto">
                     <img class="object-cover w-full" src="<?php echo get_template_directory_uri();?>/assets/images/front-page/bottom1-img2.jpg" alt="a list of users"/>
                 </div>
             </div>
     
-            <div class="lg:order-1 max-w-[512px] w-full text-center md:text-left">
+            <div class="md:order-1 max-w-[512px] w-full text-center md:text-left">
                 <span class="text-blue">Features</span>
                 <h2 class="mb-6 mt-4 lg:text-h2 md:text-h4 text-h5 text-primary">Stay connected with your customers</h2>
                 <p>Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Ut nec turpis tristique, egestas lacus at, cursus arcu</p>

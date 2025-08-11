ac28291b1fdb85790d864c07877395181d565ef0
Mark-Luigi <markluigibatoks@gmail.com>
2025-08-11 04:53:24 -0230

    add: about-us/section2

---
+++ partials/about-us/section2.php
@@ -0,0 +1,138 @@
+<div class="main-box bg-white">
+    <div class="wrapper">
+      <div class="lg:flex gap-5 justify-between">
+        <div class="lg:max-w-[282px] w-full text-center lg:text-left">
+            <h2 class="lg:mb-6 mb-5 lg:text-h2 md:text-h4 text-h5 text-primary">Our Team</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur dipiscing elit.</p>
+        </div>
+
+        <div class="lg:mt-0 mt-10 grid grid-cols-2 md:grid-cols-3 gap-y-20 gap-x-10">
+          <div class="w-full">
+            <div class="mb-8 max-w-max">
+              <img class="rounded-[10px] object-cover w-full" src="<?php echo get_template_directory_uri();?>/assets/images/about-us/section2-img1.jpg" alt="drogo blake"/>
+            </div>
+            <span class="text-blue block text-sm">CEO</span>
+            <h2 class="mt-2 mb-8 text-black font-medium text-lg md:text-[22px] leading-[28px] tracking-normal">Drogo Blake</h2>
+            <div class="flex gap-6 items-center">
+              <a href="#" aria-label="dummy-link" target="_blank" class="hover:opacity-78 w-4 h-4 display flex items-center justify-center">
+                <?php echo inline_svg('/assets/images/facebook.svg'); ?>
+              </a>
+
+              <a href="#" aria-label="dummy-link" target="_blank" class="hover:opacity-78 w-4 h-4 display flex items-center justify-center">
+                <?php echo inline_svg('/assets/images/instagram.svg'); ?>
+              </a>
+
+              <a href="#" aria-label="dummy-link" target="_blank" class="hover:opacity-78 w-4 h-4 display flex items-center justify-center">
+                <?php echo inline_svg('/assets/images/twitter.svg'); ?>
+              </a>
+            </div>
+          </div>
+
+          <div class="w-full">
+            <div class="mb-8 max-w-max">
+              <img class="rounded-[10px] object-cover w-full" src="<?php echo get_template_directory_uri();?>/assets/images/about-us/section2-img2.jpg" alt="Taylor Rose"/>
+            </div>
+            <span class="text-blue block text-sm">Co - Founder</span>
+            <h2 class="mt-2 mb-8 text-black font-medium text-lg md:text-[22px] leading-[28px] tracking-normal">Taylor Rose</h2>
+            <div class="flex gap-6 items-center">
+              <a href="#" aria-label="dummy-link" target="_blank" class="hover:opacity-78 w-4 h-4 display flex items-center justify-center">
+                <?php echo inline_svg('/assets/images/facebook.svg'); ?>
+              </a>
+
+              <a href="#" aria-label="dummy-link" target="_blank" class="hover:opacity-78 w-4 h-4 display flex items-center justify-center">
+                <?php echo inline_svg('/assets/images/instagram.svg'); ?>
+              </a>
+
+              <a href="#" aria-label="dummy-link" target="_blank" class="hover:opacity-78 w-4 h-4 display flex items-center justify-center">
+                <?php echo inline_svg('/assets/images/twitter.svg'); ?>
+              </a>
+            </div>
+          </div>
+
+          <div class="w-full">
+            <div class="mb-8 max-w-max">
+              <img class="rounded-[10px] object-cover w-full" src="<?php echo get_template_directory_uri();?>/assets/images/about-us/section2-img3.jpg" alt="Page Ortega"/>
+            </div>
+            <span class="text-blue block text-sm">Head of Marketing</span>
+            <h2 class="mt-2 mb-8 text-black font-medium text-lg md:text-[22px] leading-[28px] tracking-normal">Page Ortega</h2>
+            <div class="flex gap-6 items-center">
+              <a href="#" aria-label="dummy-link" target="_blank" class="hover:opacity-78 w-4 h-4 display flex items-center justify-center">
+                <?php echo inline_svg('/assets/images/facebook.svg'); ?>
+              </a>
+
+              <a href="#" aria-label="dummy-link" target="_blank" class="hover:opacity-78 w-4 h-4 display flex items-center justify-center">
+                <?php echo inline_svg('/assets/images/instagram.svg'); ?>
+              </a>
+
+              <a href="#" aria-label="dummy-link" target="_blank" class="hover:opacity-78 w-4 h-4 display flex items-center justify-center">
+                <?php echo inline_svg('/assets/images/twitter.svg'); ?>
+              </a>
+            </div>
+          </div>
+
+          <div class="w-full">
+            <div class="mb-8 max-w-max">
+              <img class="rounded-[10px] object-cover w-full" src="<?php echo get_template_directory_uri();?>/assets/images/about-us/section2-img4.jpg" alt="Aubrey Harris"/>
+            </div>
+            <span class="text-blue block text-sm">Head of Design</span>
+            <h2 class="mt-2 mb-8 text-black font-medium text-lg md:text-[22px] leading-[28px] tracking-normal">Aubrey Harris</h2>
+            <div class="flex gap-6 items-center">
+              <a href="#" aria-label="dummy-link" target="_blank" class="hover:opacity-78 w-4 h-4 display flex items-center justify-center">
+                <?php echo inline_svg('/assets/images/facebook.svg'); ?>
+              </a>
+
+              <a href="#" aria-label="dummy-link" target="_blank" class="hover:opacity-78 w-4 h-4 display flex items-center justify-center">
+                <?php echo inline_svg('/assets/images/instagram.svg'); ?>
+              </a>
+
+              <a href="#" aria-label="dummy-link" target="_blank" class="hover:opacity-78 w-4 h-4 display flex items-center justify-center">
+                <?php echo inline_svg('/assets/images/twitter.svg'); ?>
+              </a>
+            </div>
+          </div>
+
+          <div class="w-full">
+            <div class="mb-8 max-w-max">
+              <img class="rounded-[10px] object-cover w-full" src="<?php echo get_template_directory_uri();?>/assets/images/about-us/section2-img5.jpg" alt="Isabella Bradley"/>
+            </div>
+            <span class="text-blue block text-sm">Marketer</span>
+            <h2 class="mt-2 mb-8 text-black font-medium text-lg md:text-[22px] leading-[28px] tracking-normal">Isabella Bradley</h2>
+            <div class="flex gap-6 items-center">
+              <a href="#" aria-label="dummy-link" target="_blank" class="hover:opacity-78 w-4 h-4 display flex items-center justify-center">
+                <?php echo inline_svg('/assets/images/facebook.svg'); ?>
+              </a>
+
+              <a href="#" aria-label="dummy-link" target="_blank" class="hover:opacity-78 w-4 h-4 display flex items-center justify-center">
+                <?php echo inline_svg('/assets/images/instagram.svg'); ?>
+              </a>
+
+              <a href="#" aria-label="dummy-link" target="_blank" class="hover:opacity-78 w-4 h-4 display flex items-center justify-center">
+                <?php echo inline_svg('/assets/images/twitter.svg'); ?>
+              </a>
+            </div>
+          </div>
+
+          <div class="w-full">
+            <div class="mb-8 max-w-max">
+              <img class="rounded-[10px] object-cover w-full" src="<?php echo get_template_directory_uri();?>/assets/images/about-us/section2-img6.jpg" alt="Basil Rice"/>
+            </div>
+            <span class="text-blue block text-sm">Sales Manager</span>
+            <h2 class="mt-2 mb-8 text-black font-medium text-lg md:text-[22px] leading-[28px] tracking-normal">Basil Rice</h2>
+            <div class="flex gap-6 items-center">
+              <a href="#" aria-label="dummy-link" target="_blank" class="hover:opacity-78 w-4 h-4 display flex items-center justify-center">
+                <?php echo inline_svg('/assets/images/facebook.svg'); ?>
+              </a>
+
+              <a href="#" aria-label="dummy-link" target="_blank" class="hover:opacity-78 w-4 h-4 display flex items-center justify-center">
+                <?php echo inline_svg('/assets/images/instagram.svg'); ?>
+              </a>
+
+              <a href="#" aria-label="dummy-link" target="_blank" class="hover:opacity-78 w-4 h-4 display flex items-center justify-center">
+                <?php echo inline_svg('/assets/images/twitter.svg'); ?>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+</div>

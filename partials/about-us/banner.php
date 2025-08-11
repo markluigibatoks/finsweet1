4e1dacecc142722e6833517cbcbcdcc83259b5d8
Mark-Luigi <markluigibatoks@gmail.com>
2025-08-11 04:24:56 -0230

    add: about-us/section1

--- partials/about-us/banner.php
+++ partials/about-us/banner.php
@@ -1,11 +1,11 @@
-<main class="main-box sm:pt-24 sm:pb-32 py-10">
+<main class="z-10 main-box sm:pt-24 sm:pb-32 py-10">
     <div class="wrapper relative">
         <div class="sm:max-w-[363px] md:max-w-[463px] w-full text-center sm:text-left">
             <h2 class="text-primary lg:mb-6 mb-5 xl:text-h1 lg:text-h2 md:text-h4 text-h5">About Us </h2>
             <p>Lorem ipsum dolor sit amet, consectetur dipiscing elit. Vestibulum vitae nunc vitae neque .</p>
         </div>
 
-        <div class="absolute -top-5 sm:top-[inherit] sm:-bottom-51 right-5 w-10 h-10 sm:h-[278px] sm:w-full max-w-max xl:right-29 flex items-center justify-center">
+        <div class="absolute -top-5 sm:top-[inherit] sm:-bottom-51 right-5 w-10 h-10 sm:h-[178px] sm:w-[108px] lg:h-[278px] lg:w-full max-w-max xl:right-29 flex items-center justify-center">
             <?php echo file_get_contents(get_template_directory() . '/assets/images/about-us/banner.svg'); ?>
         </div>
     </div>
--- partials/about-us/section1.php
+++ partials/about-us/section1.php
@@ -1,10 +1,20 @@
-<div class="main-box lg:py-32 py-10">
+<div class="main-box bg-white lg:py-32 py-10">
     <div class="wrapper">
-        <div class="w-full text-center">
-            <span>Subheading</span>
-            <h2>About Us</h2>
-            <p>We are a company that values excellence and innovation.</p>
-            <a href="#" class="button-primary mx-auto lg:mx-0">Learn More</a>
+        <div class="lg:max-w-[851px] w-full text-center lg:text-left">
+            <h2 class="lg:mb-6 mb-5 text-primary lg:text-h2 md:text-h4 text-h5">Finsweet</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ut nulla eu lorem rutrum tempus. Integer tincidunt tellus quis est tincidunt auctor.</p>
         </div>
+
+        <div class="relative my-10 lg:my-20 max-w-max mx-auto">
+            <img class="aspect-square sm:aspect-video md:aspect-auto rounded-tr-4xl object-cover w-full" src="<?php echo get_template_directory_uri();?>/assets/images/about-us/section1-img1.jpg" alt="a man is working seriously in an office setting"/>
+
+            <div class="absolute xl:-bottom-24 lg:-bottom-16 right-3 w-10 h-10 lg:w-full max-w-max lg:h-[153px] bottom-3 flex items-center justify-center">
+                <?php echo file_get_contents(get_template_directory() . '/assets/images/about-us/section1-icon1.svg'); ?>
+            </div>
+        </div>
+
+        <p class="max-w-[1054px] text-h5 leading-[34px]">Phasellus scelerisque eros felis, ut lobortis ipsum mattis ullamcorper. Morbi magna orci, ornare vel auctor non, malesuada sed dolor. Pellentesque facilisis condimentum nunc, nec placerat eros aliquam lobortis. Nullam non bibendum neque, nec sagittis risus. Proin est metus, bibendum at maximus quis, placerat id sapien. Fusce ipsum quam, placerat sit amet mauris vitae, suscipit ultrices turpis. Etiam ac eros est. Quisque id rhoncus dolor, eget gravida ante. Integer commodo id lacus at fringilla.</p>
+
+        <p class="mt-8 max-w-[1054px] text-black text-h5 font-semibold leading-[34px]">Phasellus scelerisque eros felis, ut lobortis ipsum mattis ullamcorper. Morbi magna orci, ornare vel auctor non, malesuada sed dolor. Pellentesque facilisis</p>
     </div>
 </div>

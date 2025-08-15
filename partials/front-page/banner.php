a4be8a489480f163216dec9811d0918e41ab40b5
Mark-Luigi <markluigibatoks@gmail.com>
2025-08-15 06:41:20 -0230

    add: 404

--- partials/front-page/banner.php
+++ partials/front-page/banner.php
@@ -4,7 +4,7 @@
             <div class="lg:max-w-[585px] w-full text-center lg:text-left">
                 <h2 class="text-primary lg:mb-8 mb-5 xl:text-h1 lg:text-h2 md:text-h4 text-h5">Run Your Entire Customer Support Remotely</h2>
                 <p>Lorem ipsum dolohr sit amet, consectetur adipiscing elit. Vestibulum vitae nunc vitae neque</p>
-                <a href="#" class="mt-12 button-blue block max-w-max mx-auto lg:mx-0">Get Started For Free</a>
+                <a href="<?php echo site_url('/contact') ?>" class="mt-12 button-blue block max-w-max mx-auto lg:mx-0">Get Started For Free</a>
             </div>
 
             <div class="xl:-mr-14 max-w-max mx-auto">
